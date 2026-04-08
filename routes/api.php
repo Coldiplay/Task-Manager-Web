47a24ebdd52817db813a55b282ba644961bf18b9
Coldiplay <cvaba2005@gmail.com>
2026-04-08 17:14:13 +1000

    Initial
    Signed-off-by: Coldiplay <cvaba2005@gmail.com>

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+*/
+
+// Swagger Documentation - редирект на Swagger UI
+Route::get('/docs', function () {
+    return redirect()->to('/spectrum/openapi.html');
+});
+
+
+// Protected routes (требуют авторизации)
+Route::middleware('auth:sanctum')->group(function () {
+
+});
