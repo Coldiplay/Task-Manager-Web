304620c185af2812a455931db06575e34084bba3
Coldiplay <cvaba2005@gmail.com>
2026-04-15 13:42:40 +1000

    feat: Added authorization and corresponding actions (create user, delete user, authorize, reset password, logout)

---
+++ app/Http/Requests/Auth/NewPasswordRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules;
+
+#Obsolete
+class NewPasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+            'token' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ];
+    }
+}
