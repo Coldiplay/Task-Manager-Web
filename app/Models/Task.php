02ae7d0c9a85a2a90c1aeb34eb79ccfca50c906f
Bomber32112 <bomber32112@gmail.com>
2026-06-17 14:16:39 +1000

    Fixed db

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,34 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Builder;
 
 class Task extends Model
 {
     /** @use HasFactory<\Database\Factories\TaskFactory> */
     use HasFactory;
-
-    public function project(): BelongsTo
-    {
-        return $this->belongsTo(Project::class);
-    }
-
-    public function author(): BelongsTo
-    {
-        return $this->belongsTo(User::class);
-    }
-
-    public function comments() : HasMany
-    {
-        return $this->hasMany(Comment::class);
-    }
-
-    public function assignee(): BelongsTo
-    {
-        return $this->belongsTo(User::class);
-    }
 
     //Status filter
     public function scopeByStatus(Builder $query, ?string $status): Builder
@@ -59,7 +39,6 @@ class Task extends Model
 
     public function scopeWithSorting(Builder $query, ?string $sortBy, ?string $direction = 'asc'): Builder
     {
-        return $this->belongsTo(User::class);
         // Access for the sorting
         $allowedSortFields = ['created_at', 'priority', 'due_date'];
 
@@ -73,4 +52,35 @@ class Task extends Model
             return $q->orderBy('created_at');//, 'desc');
         });
     }
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function assignee(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function comments()
+    {
+        // task may have many comments
+        return $this->hasMany(Comment::class)->latest(); // latest() sort from new to old
+    }
 }
+
+//public function __construct(string $title, string $description, string $status, string $priority, ) : task
+//{
+//    $newTask = new task();
+//    $newTask->title = $title;
+//    $newTask->description = $description;
+//    $newTask->status  = $status;
+//
+//
+//    return $newTask;
+//}
